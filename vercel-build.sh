d4d5802d61bbeed861e63b7e7190de10967452b1
jahmezzz <swcaiagent@gmail.com>
2025-11-15 07:33:09 +0100

    vercel update

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-# Install dependencies
+# --- Install dependencies ---
 apt-get update && apt-get install -y curl unzip xz-utils git
 
-# Download Flutter SDK
-curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.2-stable.tar.xz
+# --- Download Flutter 3.24.3 (supports Dart 3.5) ---
+curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.3-stable.tar.xz
 
-# Extract Flutter
-tar xf flutter_linux_3.22.2-stable.tar.xz
+# --- Extract Flutter ---
+tar xf flutter_linux_3.24.3-stable.tar.xz
 
-# Add Flutter to PATH
+# --- Add Flutter to PATH ---
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Enable web
+# --- Allow git safe directory ---
+git config --global --add safe.directory /vercel/path0/flutter
+
+# --- Enable web ---
 flutter config --enable-web
 
-# Get packages
+# --- Get dependencies ---
 flutter pub get
 
-# Build web release
+# --- Build ---
 flutter build web --release
