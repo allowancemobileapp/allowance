709d21dcaf54b6a9cedb16a518090ff54cb89c8d
jahmezzz <swcaiagent@gmail.com>
2025-11-17 05:35:24 +0100

    Remove sudo from install script

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update
-sudo apt-get install -y xz-utils curl git
+apt-get update
+apt-get install -y xz-utils curl git
 
 curl -LO https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.0-stable.tar.xz
 tar -xf flutter_linux_3.24.0-stable.tar.xz
